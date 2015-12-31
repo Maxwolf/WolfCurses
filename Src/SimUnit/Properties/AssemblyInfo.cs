0b4e1d7573f72bf54d25915cb6a78047bbaf1c8c
Maxwolf <ron.mcdowell@gmail.com>
2015-12-31 04:57:37 -0800

    Initial commit to separate the console manager from the project I created it from so I may abstract it away from there and make use of it again the future.

---
+++ Src/SimUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimUnit")]
+[assembly: AssemblyDescription("SimUnit")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SimUnit")]
+[assembly: AssemblyProduct("SimUnit")]
+[assembly: AssemblyCopyright("SimUnit")]
+[assembly: AssemblyTrademark("SimUnit")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dad43fa9-4823-4c23-a9b0-edb0fa036db4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Src/SimUnit/Window/WindowData.cs
@@ -0,0 +1,13 @@
+﻿// Created by Ron 'Maxwolf' McDowell (ron.mcdowell@gmail.com) 
+// Timestamp 12/07/2015@3:31 AM
+
+namespace SimUnit
+{
+    /// <summary>
+    ///     Used to make sure that every Windows info class has a basic data structure that we can rely on for creating it via
+    ///     Windows factory.
+    /// </summary>
+    public abstract class WindowData
+    {
+    }
+}
